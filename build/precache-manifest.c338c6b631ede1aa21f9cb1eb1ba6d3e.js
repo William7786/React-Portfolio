f196afb3b20cc72e7b2f8360ae2e96d2ade7562e
Tcomp <williamt7786@gmail.com>
2021-08-22 23:29:19 -0400

    for hopepage fix

---
+++ build/precache-manifest.c338c6b631ede1aa21f9cb1eb1ba6d3e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f452a02a793dcbca15e8b2e2c55755",
+    "url": "/William7786/React-Portfolio/index.html"
+  },
+  {
+    "revision": "517b7a7660e5bc1c4938",
+    "url": "/William7786/React-Portfolio/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "a6b7648af70e66f2ff08",
+    "url": "/William7786/React-Portfolio/static/css/main.b06c5fc7.chunk.css"
+  },
+  {
+    "revision": "517b7a7660e5bc1c4938",
+    "url": "/William7786/React-Portfolio/static/js/2.3e78d9e7.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/William7786/React-Portfolio/static/js/2.3e78d9e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6b7648af70e66f2ff08",
+    "url": "/William7786/React-Portfolio/static/js/main.b2d1e090.chunk.js"
+  },
+  {
+    "revision": "404879a67ef75ec1c2f8",
+    "url": "/William7786/React-Portfolio/static/js/runtime-main.de8c2de3.js"
+  },
+  {
+    "revision": "efe74239da056e4d07301bd34246b420",
+    "url": "/William7786/React-Portfolio/static/media/Olympic-Games-.efe74239.png"
+  },
+  {
+    "revision": "db65ed9438273cb661b9cee9464e1782",
+    "url": "/William7786/React-Portfolio/static/media/Password-Generator.db65ed94.png"
+  },
+  {
+    "revision": "bc1308cb7e900ff34de10c9f782f6237",
+    "url": "/William7786/React-Portfolio/static/media/Work-Day-Scheduler.bc1308cb.png"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/William7786/React-Portfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/William7786/React-Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/William7786/React-Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/William7786/React-Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/William7786/React-Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/William7786/React-Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3fd0bc972cc1c29b4eb4512fd78c80dc",
+    "url": "/William7786/React-Portfolio/static/media/image1.3fd0bc97.png"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/William7786/React-Portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/William7786/React-Portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/William7786/React-Portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/William7786/React-Portfolio/static/media/ionicons.621bd386.svg"
+  }
+]);
